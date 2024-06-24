715de47d7e2f8a5bd6ba9d74db80b496403823ad
BingBong <bingbong@pancakeswap.com>
2024-06-24 18:14:21 +0100

    feat: implement wagmi and tanstack query

---
+++ apps/frontend/src/config/chains.ts
@@ -0,0 +1,32 @@
+import { ChainId } from "@pancakeswap/chains";
+import {
+  sepolia,
+  mainnet,
+  type Chain,
+  neonDevnet,
+  arbitrumSepolia,
+} from "viem/chains";
+
+export const CHAINS: [Chain, ...Chain[]] = [
+  mainnet,
+  sepolia,
+  neonDevnet,
+  arbitrumSepolia,
+];
+
+export const PUBLIC_NODES: Record<number, string[] | readonly string[]> = {
+  [ChainId.ETHEREUM]: [
+    "https://ethereum.publicnode.com",
+    "https://eth.llamarpc.com",
+    "https://cloudflare-eth.com",
+  ].filter(Boolean),
+  [ChainId.SEPOLIA]: ["https://rpc.sepolia.org"],
+  [ChainId.ARBITRUM_SEPOLIA]: arbitrumSepolia.rpcUrls.default.http as any,
+} satisfies Record<number, string[]>;
+
+export const ChainsAdapter: { [chainId: number]: Chain } = {
+  [ChainId.SEPOLIA]: sepolia,
+  [ChainId.ARBITRUM_SEPOLIA]: arbitrumSepolia,
+};
+
+export const allChains = Object.values(ChainsAdapter);
---
+++ apps/frontend/src/config/wagmiConfig.ts
@@ -0,0 +1,72 @@
+import { createConfig, fallback, http } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { CHAINS, PUBLIC_NODES } from "./chains";
+
+import type { Transport } from "viem";
+import { injected } from "wagmi/connectors";
+
+export const noopStorage = {
+  getItem: (_key: never) => "",
+  setItem: (_key: never, _value: never) => null,
+  removeItem: (_key: never) => null,
+};
+
+export const injectedConnector = injected({
+  shimDisconnect: false,
+});
+export const metaMaskConnector = injected({
+  target: "metaMask",
+  shimDisconnect: false,
+});
+
+const PUBLIC_MAINNET = "https://ethereum.publicnode.com";
+
+export const chains = CHAINS;
+
+export const transports = chains.reduce(
+  (ts, chain) => {
+    let httpStrings: string[] | readonly string[] | undefined = [];
+
+    if (process.env.NODE_ENV === "test" && chain.id === mainnet.id) {
+      httpStrings = [PUBLIC_MAINNET];
+    } else {
+      httpStrings = PUBLIC_NODES[chain.id] ? PUBLIC_NODES[chain.id] : [];
+    }
+
+    if (ts && httpStrings) {
+      return {
+        // biome-ignore lint/performance/noAccumulatingSpread: <explanation>
+        ...ts,
+        [chain.id]: fallback(httpStrings.map((t) => http(t))),
+      };
+    }
+
+    return {
+      [chain.id]: fallback(
+        (httpStrings as string[] | readonly string[]).map((t) => http(t)),
+      ),
+    };
+  },
+  {} as Record<number, Transport>,
+);
+
+export const CLIENT_CONFIG = {
+  batch: {
+    multicall: {
+      batchSize: 1024 * 200,
+      wait: 16,
+    },
+  },
+  pollingInterval: 6_000,
+};
+
+export function createWagmiConfig() {
+  return createConfig({
+    chains,
+    ssr: true,
+    syncConnectedChain: true,
+    transports,
+    ...CLIENT_CONFIG,
+    connectors: [metaMaskConnector, injectedConnector],
+  });
+}
