3b9fd812fccd3daeb98fe9ffdf9bb428dc2a73ee
BingBong <bingbong@pancakeswap.com>
2024-06-24 19:24:53 +0100

    chore: seperate styles from nav bar file

--- apps/frontend/src/components/Navbar/Navbar.tsx
+++ apps/frontend/src/components/Navbar/Navbar.tsx
@@ -1,97 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useCallback, useEffect, useState } from "react";
-import styled, { css } from "styled-components";
+import { useCallback } from "react";
+import styled from "styled-components";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
-import PrimaryButton from "../Button/PrimaryButton/PrimaryButton";
 import { shortenAddress } from "~/utils/misc";
-import { UilAngleDown } from "@iconscout/react-unicons";
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  width: 100%;
-  justify-content: space-between;
-  position: fixed;
-  top: 0px;
-  z-index: 100;
-  /* background: rgb(12, 18, 43); */
-
-  ${(props: any) =>
-    props.isNavbarDark &&
-    css`
-      background: rgb(12, 18, 43);
-    `}
-`;
-
-export const Nav = styled.nav`
-  padding: 24px 16px;
-  width: 100%;
-  height: 72px;
-  z-index: 2;
-  box-sizing: border-box;
-  display: block;
-`;
-
-export const Box = styled.div`
-  box-sizing: border-box;
-  vertical-align: initial;
-  -webkit-tap-highlight-color: transparent;
-  display: flex;
-  flex-wrap: nowrap;
-  height: 100%;
-`;
-
-export const BoxItemContainer = styled.div`
-  box-sizing: border-box;
-  vertical-align: initial;
-  -webkit-tap-highlight-color: transparent;
-  justify-content: ${(props: any) => props.allignment};
-  display: flex;
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex-basis: 0;
-  width: 100%;
-  align-items: center;
-`;
+import PrimaryButton from "../Button/PrimaryButton/PrimaryButton";
+import { Box, BoxItemContainer, Nav, Wrapper } from "./styles";
 
 const ROUTES: string[] = ["swap"];
 
-const NavLinks = ({ routes }: { routes: string[] }) => {
-  return (
-    <>
-      {routes.map((route: string) => {
-        return (
-          <Link
-            href={`/${route === "home" ? "" : route}`}
-            key={route}
-            className="mx-1 hidden flex-row items-center gap-2 md:flex"
-          >
-            <span className=" w-full rounded-xl px-4 py-2 text-center text-[20px] hover:cursor-pointer">
-              {route}
-            </span>
-          </Link>
-        );
-      })}
-    </>
-  );
-};
-
 export const Navbar = () => {
-  const [isNavbarDark, setIsNavbarDark] = useState(false);
   const { address: account, isConnected: active } = useAccount();
   const { connectAsync, connectors } = useConnect();
-
   const { disconnect } = useDisconnect();
-
-  const changeBackground = () => {
-    if (window.scrollY >= 66) {
-      setIsNavbarDark(true);
-    } else {
-      setIsNavbarDark(false);
-    }
-  };
 
   const enableConnection = useCallback(async () => {
     if (!connectors[0]) return;
@@ -107,14 +28,8 @@ export const Navbar = () => {
     disconnect();
   }, [disconnect]);
 
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-    changeBackground();
-    window.addEventListener("scroll", changeBackground);
-  });
-
   return (
-    <Wrapper isNavbarDark={isNavbarDark}>
+    <Wrapper>
       <Nav>
         <Box>
           <BoxItemContainer allignment={"flex-start"}>
@@ -158,6 +73,26 @@ export const Navbar = () => {
         </Box>
       </Nav>
     </Wrapper>
+  );
+};
+
+const NavLinks = ({ routes }: { routes: string[] }) => {
+  return (
+    <>
+      {routes.map((route: string) => {
+        return (
+          <Link
+            href={`/${route === "home" ? "" : route}`}
+            key={route}
+            className="mx-1 hidden flex-row items-center gap-2 md:flex"
+          >
+            <span className=" w-full rounded-xl px-4 py-2 text-center text-[20px] hover:cursor-pointer">
+              {route}
+            </span>
+          </Link>
+        );
+      })}
+    </>
   );
 };
 
---
+++ apps/frontend/src/components/Navbar/styles.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0px;
+  z-index: 100;
+`;
+
+export const Nav = styled.nav`
+  padding: 24px 16px;
+  width: 100%;
+  height: 72px;
+  z-index: 2;
+  box-sizing: border-box;
+  display: block;
+`;
+
+export const Box = styled.div`
+  box-sizing: border-box;
+  vertical-align: initial;
+  -webkit-tap-highlight-color: transparent;
+  display: flex;
+  flex-wrap: nowrap;
+  height: 100%;
+`;
+
+export const BoxItemContainer = styled.div`
+  box-sizing: border-box;
+  vertical-align: initial;
+  -webkit-tap-highlight-color: transparent;
+  justify-content: ${(props: any) => props.allignment};
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: 0;
+  width: 100%;
+  align-items: center;
+`;
